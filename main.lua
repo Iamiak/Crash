a7b747550b5f737a8c079bbe3ccc80618f3afff7
Iamiakk <153729760+Iamiak@users.noreply.github.com>
2024-03-04 11:15:40 +0530

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+function wait(seconds)
+  local start = os.time()
+  while os.time() - start < seconds do
+  end
+print("      _                _                       ")
+print(" /\  | |              | |                      ")
+print("/  \ | |_ ___  _ __   | |_ ___  __ _ _ __ ___  ")
+print("/ /\ \| __/ _ \\| '_ \\  | __/ _ \\ / _` | '_ ` _ \\ ")
+print("/ ____ \\ || (_) | |_) | | ||  __/ (_| | | | | | |")
+print("/_/    \\_\\__\\___/| .__/   \\__\\___|\\__,_|_| |_| |_|")
+print("                | |                              ")
+print("                |_|                              ")
+
+  local Crash = true
+  if Crash == true then
+    wait(2) 
+    repeat
+      print("Get Lemon'd") 
+    until Crash == true
+  end
+    
