1e10664c43d9a6b0f07c619c94ac3ba4b47d0a06
Iamiakk <153729760+Iamiak@users.noreply.github.com>
2024-03-04 11:49:24 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,21 +1,40 @@
 function wait(seconds)
   local start = os.time()
   while os.time() - start < seconds do
+    -- do nothing, just wait
   end
-print("      _                _                       ")
-print(" /\  | |              | |                      ")
-print("/  \ | |_ ___  _ __   | |_ ___  __ _ _ __ ___  ")
-print("/ /\ \| __/ _ \\| '_ \\  | __/ _ \\ / _` | '_ ` _ \\ ")
-print("/ ____ \\ || (_) | |_) | | ||  __/ (_| | | | | | |")
-print("/_/    \\_\\__\\___/| .__/   \\__\\___|\\__,_|_| |_| |_|")
-print("                | |                              ")
-print("                |_|                              ")
+end
+print("    _  _              _                  ")
+print("   /_\\| |_ ___ _ __  | |_ ___ __ _ _ __  ")
+print("  / _ \\  _/ _ \\ '_ \\ |  _/ -_) _` | '  \\ ")
+print(" /_/ \\_\\__\\___/ .__/  \\__\\___\\__,_|_|_|_|")
+print("              |_|                         ")
 
-  local Crash = true
-  if Crash == true then
-    wait(2) 
-    repeat
-      print("Get Lemon'd") 
-    until Crash == true
-  end
-    
+
+local Crash = true
+
+local Crashdo = true
+
+local troll = true
+
+wait(3)
+if Crash == true then
+  wait(2) 
+  repeat
+    print("Get Lemon'd") 
+  until Crash == false
+end
+
+if Crashdo == true then
+  wait(2) 
+  repeat
+    print("LOL") 
+  until Crash == false
+end
+
+if Cra == true then
+  wait(2) 
+  repeat
+    print("NOOB") 
+  until Crash == false
+end
